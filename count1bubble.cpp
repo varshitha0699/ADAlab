78b6da1933f1157916f7985a5222af813def054d
varshitha0699 <43058154+varshitha0699@users.noreply.github.com>
2019-08-31 11:08:54 +0530

    Add files via upload

---
+++ count1bubble.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+//clock_t start,end;
+
+int main()
+{
+	int a[10],n,k,temp,count=0;
+	clock_t start;
+	clock_t end;
+	start=clock();
+	cout<<"enter n";
+	cin>>n;
+	cout<<"enter elements:";
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<"enter k";
+	cin>>k;
+	for(int i=0;i<k;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			
+			
+			if(a[j]>a[j+1])
+			{
+				count++
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	cout<<"small:"<<a[k-1];
+	end=clock();
+	cout<<"\ntime:"<<(double)(end-start)/(double)CLOCKS_PER_SEC;
+	cout<<"\ncount:"<<count;
+}
